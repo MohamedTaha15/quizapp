5c386ce34f810b71973dee6987bd8a91d46be960
MohamedTaha <mohamedtaha.9718@gmail.com>
2024-01-08 16:34:10 +0200

    saving questions to database before run and return all questions

--- src/main/java/com/example/quizapp/question/Question.java
+++ src/main/java/com/example/quizapp/question/Question.java
@@ -17,7 +17,7 @@ public class Question {
     )
     @GeneratedValue(strategy = GenerationType.SEQUENCE,
                     generator = "question_sequence")
-    private Long id;
+    private Integer id;
     private String questionTitle;
     private String option1;
     private String option2;
@@ -26,5 +26,25 @@ public class Question {
     private String rightAnswer;
     private String difficultyLevel;
 
+
+    public Question(String questionTitle,
+                    String option1,
+                    String option2,
+                    String option3,
+                    String option4,
+                    String rightAnswer,
+                    String difficultyLevel) {
+        this.questionTitle = questionTitle;
+        this.option1 = option1;
+        this.option2 = option2;
+        this.option3 = option3;
+        this.option4 = option4;
+        this.rightAnswer = rightAnswer;
+        this.difficultyLevel = difficultyLevel;
+    }
+
+    public Question() {
+
+    }
 
 }
--- src/main/java/com/example/quizapp/question/QuestionConfig.java
+++ src/main/java/com/example/quizapp/question/QuestionConfig.java
@@ -1,8 +1,96 @@
 package com.example.quizapp.question;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class QuestionConfig {
 
+    @Bean
+    CommandLineRunner commandLineRunner(QuestionRepository repository){
+        return args -> {
+            Question question1 = new Question("questionTitle",
+                    "option1",
+                    "option2",
+                    "option3",
+                    "option4",
+                    "rightAnswer",
+                    "difficultyLevel");
+
+            Question question2 = new Question("questionTitle",
+                    "option1",
+                    "option2",
+                    "option3",
+                    "option4",
+                    "rightAnswer",
+                    "difficultyLevel");
+
+            Question question3 = new Question("questionTitle",
+                    "option1",
+                    "option2",
+                    "option3",
+                    "option4",
+                    "rightAnswer",
+                    "difficultyLevel");
+
+            Question question4 = new Question("questionTitle",
+                    "option1",
+                    "option2",
+                    "option3",
+                    "option4",
+                    "rightAnswer",
+                    "difficultyLevel");
+
+            Question question5 = new Question("questionTitle",
+                    "option1",
+                    "option2",
+                    "option3",
+                    "option4",
+                    "rightAnswer",
+                    "difficultyLevel");
+
+            Question question6 = new Question("questionTitle",
+                    "option1",
+                    "option2",
+                    "option3",
+                    "option4",
+                    "rightAnswer",
+                    "difficultyLevel");
+
+            Question question7 = new Question("questionTitle",
+                    "option1",
+                    "option2",
+                    "option3",
+                    "option4",
+                    "rightAnswer",
+                    "difficultyLevel");
+
+            Question question8 = new Question("questionTitle",
+                    "option1",
+                    "option2",
+                    "option3",
+                    "option4",
+                    "rightAnswer",
+                    "difficultyLevel");
+
+            Question question9 = new Question("questionTitle",
+                    "option1",
+                    "option2",
+                    "option3",
+                    "option4",
+                    "rightAnswer",
+                    "difficultyLevel");
+
+            Question question10 = new Question("questionTitle",
+                    "option1",
+                    "option2",
+                    "option3",
+                    "option4",
+                    "rightAnswer",
+                    "difficultyLevel");
+
+
+        };
+    }
 }
